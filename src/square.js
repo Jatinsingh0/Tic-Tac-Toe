ee2d8fc30e8543a4ce16daba3fa7403bfc07e279
JATIN SINGH <126510150+Jatinsingh0@users.noreply.github.com>
2023-08-16 01:05:00 +0530

    Initial commit

---
+++ src/square.js
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <>
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
+    </>
+  );
+};
+
+export default Square;
